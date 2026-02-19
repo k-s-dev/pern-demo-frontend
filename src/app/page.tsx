10fccaa2929cd5458fd5ca6d0edfabf5720e3fc5
k-s-dev <kushagra.singh@gmail.com>
2026-02-19 05:38:38 +0530

    home page content update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import AppShell from "@/lib/ui/components/layout/01/AppShell";
 import styles from "./page.module.scss";
-import { Button, Text } from "@mantine/core";
+import { Button, Flex, Text } from "@mantine/core";
 import Link from "next/link";
 import Navbar from "@/lib/ui/components/nav/Navbar";
 
@@ -13,22 +13,38 @@ export default async function Page() {
         </header>
 
         <section className={styles.stackAndFeaturesContainer}>
-          <h2>Features</h2>
-          <section className={styles.featuresRow}>
-            {features.map(([title, href], idx) => {
-              return <Item key={idx} href={href} title={title} />;
-            })}
+          <header>
+            <Flex direction={"column"} align={"flex-start"}>
+              <h2>Stack</h2>
+              <Text c={"gray"} fw={"normal"} fz="h4" ta={"left"}>
+                PERN (Postgresql Expressjs React Nodejs) stack using Nextjs app
+                router written with Typescript.
+              </Text>
+            </Flex>
+          </header>
+          <section>
+            <header>
+              <Flex direction={"column"} align={"flex-start"}>
+                <h3>Frontend</h3>
+              </Flex>
+            </header>
+            <article className={styles.stackRow}>
+              {stack.frontend.map(([title, href], idx) => {
+                return <Item key={idx} href={href} title={title} />;
+              })}
+            </article>
           </section>
-
-          <h2>Stack</h2>
-          <Text c={"gray"} fw={"bold"}>
-            PERN (Postgresql Expressjs React Nodejs) stack using Nextjs app
-            router.
-          </Text>
-          <section className={styles.stackRow}>
-            {stack.map(([title, href], idx) => {
-              return <Item key={idx} href={href} title={title} />;
-            })}
+          <section>
+            <header>
+              <Flex direction={"column"} align={"flex-start"}>
+                <h3>Backend</h3>
+              </Flex>
+            </header>
+            <article className={styles.stackRow}>
+              {stack.backend.map(([title, href], idx) => {
+                return <Item key={idx} href={href} title={title} />;
+              })}
+            </article>
           </section>
         </section>
       </main>
@@ -43,36 +59,31 @@ function Item({ href, title }: { href: string; title: string }) {
       target={href.length === 0 ? "_self" : "_blank"}
       referrerPolicy="no-referrer"
     >
-      <Button fullWidth fz="md" color="yellow.1">
+      <Button fullWidth fz="md" fw="lighter" color="yellow.1">
         {title}
       </Button>
     </Link>
   );
 }
-const features = [
-  ["Admin", ""],
-  ["Authentication", ""],
-  ["Authorization", ""],
-  ["OAuth", ""],
-  ["Sessions", ""],
-  ["Data access & control layer", ""],
-  ["Form validation: client & server", ""],
-  ["Uploads", ""],
-  ["Emails", ""],
-  ["Test: e2e", ""],
-  ["Test: unit", ""],
-  ["Test: component", ""],
-];
 
-const stack = [
-  ["Vercel", "https://www.vercel.com/"],
-  ["Nextjs", "https://www.nextjs.org/"],
-  ["Vercel Blob", "https://vercel.com/storage/blob"],
-  ["Prisma", "https://www.prisma.io/"],
-  ["Postgres", "https://www.postgresql.org"],
-  ["Better-Auth", "https://www.better-auth.com/"],
-  ["Valibot", "https://valibot.dev/"],
-  ["Mantine UI", "https://mantine.dev/"],
-  ["Cypress", "https://www.cypress.io/"],
-  ["Jest", "https://jestjs.io/"],
-];
+const stack = {
+  frontend: [
+    ["Vercel", "https://www.vercel.com/"],
+    ["Nextjs", "https://www.nextjs.org/"],
+    ["Better-Auth", "https://www.better-auth.com/"],
+    ["Valibot", "https://valibot.dev/"],
+    ["Mantine UI", "https://mantine.dev/"],
+    ["Cypress", "https://www.cypress.io/"],
+    ["Jest", "https://jestjs.io/"],
+    ["Vercel Blob", "https://vercel.com/storage/blob"],
+  ],
+  backend: [
+    ["Vercel", "https://www.vercel.com/"],
+    ["Express", "https://expressjs.com/"],
+    ["Pino", "https://getpino.io/"],
+    ["Better-Auth", "https://www.better-auth.com/"],
+    ["Prisma", "https://www.prisma.io/"],
+    ["Postgres", "https://www.postgresql.org"],
+    ["Valibot", "https://valibot.dev/"],
+  ],
+};
