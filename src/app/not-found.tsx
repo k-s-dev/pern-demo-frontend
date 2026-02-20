5bc7f27db5f193794f732e0578f3c1dff54148f1
k-s-dev <kushagra.singh@gmail.com>
2026-02-20 14:16:39 +0530

    feature: not-found init

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,5 @@
 import NotFound404 from "@/lib/ui/components/errors/NotFound404";
-import HtmlLayout from "@/lib/ui/components/layout/HtmlLayout";
 
 export default async function NotFound() {
-  return (
-    <HtmlLayout>
-      <NotFound404 />
-    </HtmlLayout>
-  );
+  return <NotFound404 />;
 }
